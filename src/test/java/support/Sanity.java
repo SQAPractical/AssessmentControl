6713e70164d8d0625cbfd213d14fb91e71b77d6d
SQAPractical <sqapractical@gmail.com>
2019-10-06 19:45:13 -0700

    fixed sanity runner

--- src/test/java/support/Sanity.java
+++ src/test/java/support/Sanity.java
@@ -1,15 +1,19 @@
 package support;
+
+
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
+
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"pretty", "html:target/cucumber-html/alltestcases", "json:target/cucumber-json-report.json"},
-        glue = {"study/qa/automation/stepDefinitions", "study/qa/automation/utils"},
-        features = {"src/main/resources/features"},
-        tags = {"@Sanity"}
+        plugin = {"pretty", "html:target/cucumber", "json:target/cucumber/report.json"},
+        features = "src/test/resources/features",
+        glue = {"definitions", "support"},
+        tags = {"@Sanity"} // same as VM option -Dcucumber.options="--tags @predefined"
 )
-
 public class Sanity {
+
 }
+
